29a0d9effecc0f7cbb1bfa810566933f18536af9
misladkova <michaela.sladkova@gmail.com>
2021-01-04 12:43:21 +0100

    define class

---
+++ src/main/java/com/company/Drink.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Drink {
+
+    private String id;
+    private String productName;
+    private double price;
+    private boolean isForAdult;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isForAdult() {
+        return isForAdult;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setForAdult(boolean forAdult) {
+        isForAdult = forAdult;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
