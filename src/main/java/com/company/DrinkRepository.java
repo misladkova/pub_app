57ed6f8045dc66fd84d29ee51a01a0db3092961e
misladkova <michaela.sladkova@gmail.com>
2021-01-06 02:22:19 +0100

    add method

--- src/main/java/com/company/DrinkRepository.java
+++ src/main/java/com/company/DrinkRepository.java
@@ -3,4 +3,6 @@ package com.company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DrinkRepository extends JpaRepository<Drink, Integer> {
+
+    Drink findByProductName(String productName);
 }
