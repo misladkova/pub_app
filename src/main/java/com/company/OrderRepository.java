842aaec580f09818b72963dded183645ed18fa28
misladkova <michaela.sladkova@gmail.com>
2021-01-09 23:46:15 +0100

    add method

--- src/main/java/com/company/OrderRepository.java
+++ src/main/java/com/company/OrderRepository.java
@@ -2,5 +2,9 @@ package com.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    Collection<Order> findByDrinkId(int drink_id);
 }
