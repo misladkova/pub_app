407b39e880344e673891dba8c0c1a631b891a55b
misladkova <michaela.sladkova@gmail.com>
2021-01-10 01:13:34 +0100

    define class

---
+++ src/main/java/com/company/OrderDTO.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class OrderDTO {
+
+    private String productName;
+    private int amount;
+    private double price;
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
---
+++ src/main/java/com/company/UserDTO.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.List;
+
+public class UserDTO {
+
+    private String username;
+    private boolean isActive;
+    private boolean isAdult;
+    private double pocket;
+    private List<OrderDTO> orders;
+
+    public void setPocket(double pocket) {
+        this.pocket = pocket;
+    }
+
+    public void setAdult(boolean adult) {
+        isAdult = adult;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getPocket() {
+        return pocket;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public List<OrderDTO> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<OrderDTO> orders) {
+        this.orders = orders;
+    }
+}
