a419b8ce40810d5798ebaea7436898381a2a5861
misladkova <michaela.sladkova@gmail.com>
2021-01-10 17:11:35 +0100

    define class

---
+++ src/main/java/com/company/OrderSumDTO.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class OrderSumDTO {
+
+    private String productName;
+    private int amount;
+    private double unitPrice;
+    private double price;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+}
